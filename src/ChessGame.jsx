e0016a5dfc80bafdf1653647522902f74373ff25
anjali gupta <sajalanjali3@gmail.com>
2024-12-30 17:26:53 +0530

    test

--- src/ChessGame.jsx
+++ src/ChessGame.jsx
@@ -127,42 +127,51 @@ const ChessGame = () => {
   };
 
   return (
-    <div style={containerStyle}>
-      <div style={boardContainerStyle}>
-        <div style={statusStyle}>{getGameStatus()}</div>
-        <Chessboard
-          position={game.fen()}
-          onPieceDrop={onDrop}
-          customBoardStyle={{
-            borderRadius: "4px",
-            boxShadow: "0 2px 10px rgba(0, 0, 0, 0.3)",
-          }}
-          customDarkSquareStyle={{ backgroundColor: "#779952" }}
-          customLightSquareStyle={{ backgroundColor: "#edeed1" }}
-        />
-        <button style={buttonStyle} onClick={resetGame}>
-          Start New Game
-        </button>
-      </div>
-      <div style={moveLogStyle}>
-        <h1>moveLog</h1>
+    <div>
+      <div className="max-[1260px] flex md:flex-row flex-col mx-auto px-10 gap-4 ">
+        <div style={boardContainerStyle}>
+          <div className="text-black text-2xl md:py-2 py-10">
+            {getGameStatus()}
+          </div>
+          <Chessboard
+            position={game.fen()}
+            onPieceDrop={onDrop}
+            customBoardStyle={{
+              borderRadius: "4px",
+              boxShadow: "0 2px 10px rgba(0, 0, 0, 0.3)",
+            }}
+            customDarkSquareStyle={{ backgroundColor: "#779952" }}
+            customLightSquareStyle={{ backgroundColor: "#edeed1" }}
+          />
+        </div>
+        <div className="py-10 border-[1] border-black  ">
+          <div className="flex gap-10 items-center">
+            <h1 className="text-2xl font-bold">Move Logs</h1>
+            <button
+              className="px-3 py-2 font-bold text-black rounded bg-blue-400"
+              onClick={resetGame}
+            >
+              Start New Game
+            </button>
+          </div>
 
-        {moveLog.length > 0 ? (
-          <table>
-            {" "}
-            <tbody>
-              {moveLog.map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{convertToIcon(item.white || "")}</td>
-                  <td>{convertToIcon(item.black || "")}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>no moves yet</p>
-        )}
+          {moveLog.length > 0 ? (
+            <table className="mt-10">
+              {" "}
+              <tbody>
+                {moveLog.map((item, index) => (
+                  <tr key={index} className="py-2">
+                    <td className="px-3">{`${index + 1}.`}</td>
+                    <td className="px-3">{convertToIcon(item.white || "")}</td>
+                    <td className="px-6">{convertToIcon(item.black || "")}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>no moves yet</p>
+          )}
+        </div>
       </div>
     </div>
   );
